0566aacf521be595a77cd33b3204a48a0ad64478
chesterrfielddd <ilya.polevshickov@gmail.com>
2025-02-16 15:49:44 +0300

    base structure is configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
+import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
