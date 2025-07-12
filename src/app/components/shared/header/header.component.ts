3b946ddf7473bb01b84f8a216e4812226fd4ab48
chesterrfielddd <ilya.polevshickov@gmail.com>
2025-07-12 19:38:00 +0300

    layout and styles corrections

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,43 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class HeaderComponent implements OnInit {
-  readonly pageWidth = window.innerWidth;
-  readonly scrollTop$$ = new BehaviorSubject<number>(0);
-  readonly isTopModified$$ = new BehaviorSubject<boolean>(this.scrollTop$$.value <= 40);
+export class HeaderComponent {
+  isMenuOpen = false;
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.scrollTop$$.next(
-      window.scrollY ||
-        document.documentElement.scrollTop ||
-        document.body.scrollTop ||
-        0
-    );
-  }
-
-  ngOnInit(): void {
-    this.initSubscribtions();
-  }
-
-  private initSubscribtions(): void {
-    this.scrollTop$$.subscribe((value) => {
-      if (value <= 40 && this.pageWidth <= 400) {
-        this.isTopModified$$.next(true);
-      }
-      if (value > 40 && this.pageWidth <= 400) {
-        this.isTopModified$$.next(false);
-      }
-      if (this.pageWidth > 400) {
-        this.isTopModified$$.next(false);
-      }
-    });
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
